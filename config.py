17484a43a0712ec18a16ffd3d408af98326d2fc6
donewiththedollar <donewiththedollar@gmail.com>
2023-01-12 20:50:26 -0500

    first

---
+++ config.py
@@ -0,0 +1,20 @@
+# Bybit API keys
+api_key = 'SZGDGDACCPBWKMFHDM'
+api_secret = 'RWDKZPJHHAJWVYHSYQIFYJFYVVLYLMCRUPAI'
+
+# Binance API Keys
+binance_api_key = ''
+binance_api_secret = ''
+
+endpoint = 'https://api.bybit.com'
+domain = 'bybit'
+config_max_size = 15000
+
+config_min_volume = 15000
+config_min_distance = 0.15
+config_high_volume = 100000
+config_high_distance = 500
+config_violent_volume = 300000
+config_violent_distance = 500
+
+config_botname = 'tylertestingsub1'
