b36c7cbe9916e9a8dfa2373dc02518044d5961a7
Tyler Simpson <89611464+donewiththedollar@users.noreply.github.com>
2023-01-21 00:45:53 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,8 @@ binance_api_secret = ''
 
 endpoint = 'https://api.bybit.com'
 domain = 'bybit'
-config_max_size = 15000
 
 config_min_volume = 15000
 config_min_distance = 0.15
-config_high_volume = 100000
-config_high_distance = 500
-config_violent_volume = 300000
-config_violent_distance = 500
 
-config_botname = 'tylertestingsub1'
+config_botname = 'botnameherefortg'
