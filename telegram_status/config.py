e5fb8cf53c75ac4ac5bc1f56e92e032149003268
donewiththedollar <donewiththedollar@gmail.com>
2023-01-21 10:32:44 -0500

    fix

---
+++ telegram_status/config.py
@@ -0,0 +1,11 @@
+# Bybit API keys
+CsssssssssH'
+api_secret = 'GLRXDAILEFXMFVERRTUAHBCITPVNZVFAWJJV'
+
+# Binance API Keys
+binance_api_key = ''
+binance_api_secret = ''
+
+endpoint = 'https://api.bybit.com'
+domain = 'bybit'
+config_max_size = 88888
