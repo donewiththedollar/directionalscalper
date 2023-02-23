025511d4eb909d81f93690ee393a0bdb713e9e84
Ed Coppen <51025241+ecoppen@users.noreply.github.com>
2023-02-23 16:08:22 +0000

    Update tylerapi.py
    - mypy ignore requests
    - remove unused imports
    - spacing

--- tylerapi.py
+++ tylerapi.py
@@ -1,6 +1,7 @@
-import requests
 import json
-from typing import TypedDict, Dict
+
+import requests  # type: ignore
+
 
 def grab_api_data():
     try:
@@ -9,7 +10,7 @@ def grab_api_data():
 
         if not tyler_api_unparsed.text:
             raise Exception("The API response is empty")
-            
+
         api_data = tyler_api_unparsed.json()
 
         return api_data
@@ -17,117 +18,141 @@ def grab_api_data():
         raise Exception("Error in retrieving data from api: {}".format(str(e))) from e
     except json.decoder.JSONDecodeError as e:
         raise Exception("Error in parsing the JSON data: {}".format(str(e))) from e
-        
+
+
 # tyler_api_unparsed = requests.get("http://13.127.240.18/data/quantdata.json")
 
 # api_data = tyler_api_unparsed.json()
 
+
 def get_asset_data(symbol, data):
     for asset in data:
-        if asset['Assets'] == symbol:
+        if asset["Assets"] == symbol:
             return asset
     return None
 
+
 def get_asset_price(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['Price']
+        return asset_data["Price"]
     return None
+
 
 # print(get_asset_price('BTCUSDT', api_data))
 
+
 def get_asset_total_volume_1m(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['1m 1x Volume (USDT)']
+        return asset_data["1m 1x Volume (USDT)"]
     return None
+
 
 # print(get_asset_total_volume_1m('BTCUSDT', api_data))
 
+
 def get_asset_volume_1m_1x(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['1m 1x Volume (USDT)']
+        return asset_data["1m 1x Volume (USDT)"]
     return None
+
 
 # print(get_asset_volume_1m_1x('BTCUSDT', api_data))
 
+
 def get_asset_total_volume_5m(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['5m 1x Volume (USDT)']
+        return asset_data["5m 1x Volume (USDT)"]
     return None
+
 
 # print(get_asset_total_volume_5m('BTCUSDT', api_data))
 
+
 def get_asset_volume_5m_1x(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['5m 1x Volume (USDT)']
+        return asset_data["5m 1x Volume (USDT)"]
     return None
+
 
 # print(get_asset_volume_5m_1x('BTCUSDT', api_data))
 
+
 def get_asset_1m_spread(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['1m Spread']
+        return asset_data["1m Spread"]
     return None
+
 
 # print(get_asset_1m_spread('BTCUSDT', api_data))
 
+
 def get_asset_5m_spread(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['5m Spread']
+        return asset_data["5m Spread"]
     return None
+
 
 # print(get_asset_5m_spread('BTCUSDT', api_data))
 
+
 def get_asset_15m_spread(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['15m Spread']
+        return asset_data["15m Spread"]
     return None
 
+
 # print(get_asset_15m_spread('BTCUSDT', api_data))
+
 
 def get_asset_30m_spread(symbol, data):
     asset_data = get_asset_data(symbol, data)
-    
+
     if asset_data:
-        return asset_data['30m Spread']
+        return asset_data["30m Spread"]
     return None
 
+
 # print(get_asset_15m_spread('BTCUSDT', api_data))
+
 
 def get_asset_trend(symbol, data):
     asset_data = get_asset_data(symbol, data)
 
     if asset_data:
-        return asset_data['Trend']
+        return asset_data["Trend"]
     return None
 
-#print(get_asset_trend('APTUSDT', api_data))
+
+# print(get_asset_trend('APTUSDT', api_data))
+
 
 def get_asset_trend_pct(symbol, data):
     asset_data = get_asset_data(symbol, data)
 
     if asset_data:
-        return asset_data['trend%']
+        return asset_data["trend%"]
     return None
+
 
 def get_asset_funding_rate(symbol, data):
     asset_data = get_asset_data(symbol, data)
 
     if asset_data:
-        return asset_data['Funding']
+        return asset_data["Funding"]
     return None
