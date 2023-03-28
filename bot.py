663ba61f24518db97a560c05bd57b70c5e80b7df
donewiththedollar <donewiththedollar@gmail.com>
2023-03-28 15:09:55 -0400

    Violent mode works but needed work

--- bot.py
+++ bot.py
@@ -437,7 +437,7 @@ if not leverage_verified:
     except Exception as e:
         print(f"An unknown error occured in leverage verification: {e}")
         log.warning(f"{e}")
-        
+
 get_balance()
 
 # max_trade_qty = round(
@@ -454,7 +454,7 @@ max_trade_qty = round(
     int(float(get_market_data()[2])),
 )
 
-violent_max_trade_qty = max_trade_qty * 10
+violent_max_trade_qty = max_trade_qty * 5
 
 current_leverage = get_market_data()[1]
 
