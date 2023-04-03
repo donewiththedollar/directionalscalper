21ac5646c9b7699435303b24ffef4b0ec4e50213
Ed Coppen <51025241+ecoppen@users.noreply.github.com>
2023-04-03 11:07:44 +0100

    add 30m timeframe

--- api/exchanges/utils.py
+++ api/exchanges/utils.py
@@ -15,6 +15,7 @@ class Intervals(str, Enum):
     ONE_MINUTE = "1m"
     FIVE_MINUTES = "5m"
     FIFTEEN_MINUTES = "15m"
+    THIRTY_MINUTES = "30m"
     ONE_HOUR = "1h"
     FOUR_HOURS = "4h"
     ONE_DAY = "1d"
