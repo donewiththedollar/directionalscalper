77019f212f7b14142f591b31eb43a58a6cbb3bf0
donewiththedollar <donewiththedollar@gmail.com>
2023-04-08 00:12:34 -0400

    Without avoidfees we can utilize this condition again

--- directionalscalper/bot.py
+++ directionalscalper/bot.py
@@ -1113,7 +1113,7 @@ def trade_func(symbol):  # noqa
                             and find_5m_spread() > min_distance
                             and short_pos_qty < max_trade_qty
                             and add_short_trade_condition()
-                            # and current_ask > short_pos_price
+                            and current_ask > short_pos_price
                         ):
                             try:
                                 exchange.create_limit_sell_order(
@@ -1129,7 +1129,7 @@ def trade_func(symbol):  # noqa
                             and find_5m_spread() > min_distance
                             and long_pos_qty < max_trade_qty
                             and add_long_trade_condition()
-                            # and current_bid < long_pos_price
+                            and current_bid < long_pos_price
                         ):
                             try:
                                 exchange.create_limit_buy_order(
