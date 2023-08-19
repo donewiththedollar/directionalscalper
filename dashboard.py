e85b6af62885679921943af63cbafdd5a992adf5
donewiththedollar <donewiththedollar@gmail.com>
2023-08-19 23:18:21 +0000

    Remodel

--- dashboard.py
+++ dashboard.py
@@ -2,10 +2,9 @@ import streamlit as st
 import pandas as pd
 import time
 import json
-
+import plotly.express as px
 
 st.set_page_config(layout="wide", page_title="Trade Simple")
-
 st.title("Bot Dashboard 🤖")
 
 def get_latest_data() -> pd.DataFrame:
@@ -43,17 +42,41 @@ def get_latest_data() -> pd.DataFrame:
     ]
     return pd.DataFrame(data, columns=columns)
 
-# Create columns
-left_column, main_column, right_column = st.columns([1, 6, 1])  # You can adjust the ratios
-
-# Display the data table in the main column
-with main_column:
-    st.write(get_latest_data())
-
-# Add auto-refresh functionality
+# Sidebar components
 refresh_rate = st.sidebar.slider("Refresh Rate (seconds)", 5, 60, 10)
 auto_refresh = st.sidebar.checkbox("Auto-Refresh", True)
 
+data = get_latest_data()
+
+# Overview Section
+st.header("Overview")
+total_balance = data["Balance"].sum()
+st.metric("Total Balance", f"${total_balance:,.2f}")
+total_long_upnl = data["Long uPNL"].sum()
+st.metric("Total Long uPNL", f"${total_long_upnl:,.2f}")
+total_short_upnl = data["Short uPNL"].sum()
+st.metric("Total Short uPNL", f"${total_short_upnl:,.2f}")
+
+# Charts Section
+st.header("Charts")
+
+# Price Trend Line Chart
+fig_price = px.line(data, x="Symbol", y="Price", title="Price Trend over Symbols")
+st.plotly_chart(fig_price)
+
+# Long and Short uPNL Bar Chart
+fig_upnl = px.bar(data, x="Symbol", y=["Long uPNL", "Short uPNL"], title="Long and Short uPNL for Symbols")
+st.plotly_chart(fig_upnl)
+
+# Balance Distribution Pie Chart
+fig_balance = px.pie(data, names="Symbol", values="Balance", title="Balance Distribution over Symbols")
+st.plotly_chart(fig_balance)
+
+# Data Table Section
+st.header("Detailed Data")
+st.write(data)
+
+# Auto-refresh logic
 if auto_refresh:
     time.sleep(refresh_rate)
     st.experimental_rerun()
@@ -64,7 +87,10 @@ if auto_refresh:
 # import time
 # import json
 
-# st.title("Bot Dashboard 🤖🌸")
+
+# st.set_page_config(layout="wide", page_title="Trade Simple")
+
+# st.title("Bot Dashboard 🤖")
 
 # def get_latest_data() -> pd.DataFrame:
 #     # Load data from JSON file
@@ -101,9 +127,12 @@ if auto_refresh:
 #     ]
 #     return pd.DataFrame(data, columns=columns)
 
-# # Display the data table
-# data = get_latest_data()
-# st.write(data)
+# # Create columns
+# left_column, main_column, right_column = st.columns([1, 6, 1])  # You can adjust the ratios
+
+# # Display the data table in the main column
+# with main_column:
+#     st.write(get_latest_data())
 
 # # Add auto-refresh functionality
 # refresh_rate = st.sidebar.slider("Refresh Rate (seconds)", 5, 60, 10)
