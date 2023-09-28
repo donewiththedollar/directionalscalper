438eb278a7f89a20bf89c911462ef62672b8d687
donewiththedollar <donewiththedollar@gmail.com>
2023-09-28 04:16:08 +0000

    Update version

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from typing import Union
 
 from pydantic import BaseModel, HttpUrl, ValidationError, validator, DirectoryPath
 
-VERSION = "v2.2.9"
+VERSION = "v2.3.0"
 
 class Exchanges(Enum):
     BYBIT = "bybit"
