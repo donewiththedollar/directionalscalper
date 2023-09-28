3e90ddd668b3a5444dcc2054f3b42050bee2a9b1
donewiththedollar <donewiththedollar@gmail.com>
2023-09-28 04:15:18 +0000

    Remove old strategies

--- multi_bot.py
+++ multi_bot.py
@@ -20,8 +20,6 @@ from directionalscalper.core.strategies.strategy import Strategy
 # Bybit rotator
 from directionalscalper.core.strategies.bybit.multi.bybit_auto_rotator import BybitAutoRotator
 from directionalscalper.core.strategies.bybit.multi.bybit_auto_rotator_mfirsi import BybitAutoRotatorMFIRSI
-from directionalscalper.core.strategies.bybit.multi.bybit_auto_hedge_maker_mfirsi_rotator import BybitAutoHedgeStrategyMakerMFIRSIRotator
-from directionalscalper.core.strategies.bybit.multi.bybit_auto_maker_mfirsi_rotator_aggressive import BybitRotatorAggressive
 from directionalscalper.core.strategies.bybit.multi.bybit_mfirsi_trend_rotator import BybitMFIRSITrendRotator
 from directionalscalper.core.strategies.bybit.multi.bybit_mfirsi_trend_rotator_ratio import BybitMFIRSITrendRatio
 from directionalscalper.core.strategies.bybit.multi.bybit_spoof_rotator import BybitSpoofRotator
@@ -46,7 +44,9 @@ def choose_strategy():
                           'bybit_auto_hedge_mfi_rotator',
                           'bybit_mfirsi_trend_rotator',
                           'bybit_rotator_aggressive',
-                          'bybit_rotator_spoof',
+                          'bybit_mm',
+                          'bybit_mm_hma',
+                          'bybit_mm_5m'
                       ]
                      )
     ]
@@ -91,17 +91,11 @@ class DirectionalMarketMaker:
         elif strategy_name.lower() == 'bybit_hedge_rotator_mfirsi':
             strategy = BybitAutoRotatorMFIRSI(self.exchange, self.manager, config.bot, symbols_allowed)
             strategy.run(symbol)
-        elif strategy_name.lower() == 'bybit_hedge_mfi_rotator':
-            strategy = BybitAutoHedgeStrategyMakerMFIRSIRotator(self.exchange, self.manager, config.bot, symbols_allowed)
-            strategy.run(symbol)
         elif strategy_name.lower() == 'bybit_mfirsi_trend_rotator':
             strategy = BybitMFIRSITrendRotator(self.exchange, self.manager, config.bot, symbols_allowed)
             strategy.run(symbol)
         elif strategy_name.lower() == 'bybit_mm_ratio':
             strategy = BybitMFIRSITrendRatio(self.exchange, self.manager, config.bot, symbols_allowed)
-            strategy.run(symbol)
-        elif strategy_name.lower() == 'bybit_rotator_aggressive':
-            strategy = BybitRotatorAggressive(self.exchange, self.manager, config.bot, symbols_allowed)
             strategy.run(symbol)
         elif strategy_name.lower() == 'bybit_rotator_spoof':
             strategy = BybitSpoofRotator(self.exchange, self.manager, config.bot, symbols_allowed)
