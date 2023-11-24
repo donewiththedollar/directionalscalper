f69c9cb42f87b7d6ff3d3c73a1df9623469a7b43
Tyler Simpson <donewiththedollar@gmail.com>
2023-11-24 18:13:35 +1100

    All possible symbols

--- api/manager.py
+++ api/manager.py
@@ -143,6 +143,42 @@ class Manager:
         """Checks if the cache has expired based on cache_life_seconds."""
         return datetime.now() > self.rotator_symbols_cache_expiry
 
+    def get_all_possible_symbols(self, max_retries: int = 5):
+        url = "http://apiv3.tradesimple.xyz/data/quantdatav2_bybit.json"
+        
+        for retry in range(max_retries):
+            delay = 2**retry  # exponential backoff
+            delay = min(58, delay)  # cap the delay to 30 seconds
+
+            try:
+                logging.info(f"Sending request to {url} (Attempt: {retry + 1})")
+                header, raw_json = send_public_request(url=url)
+                
+                if isinstance(raw_json, list):
+                    logging.info(f"Received {len(raw_json)} symbols from API")
+                    
+                    symbols = [asset.get("Asset", "") for asset in raw_json if "Asset" in asset]
+                    logging.info(f"Returning {len(symbols)} symbols")
+                    return symbols
+                else:
+                    logging.warning("Unexpected data format. Expected a list of symbols.")
+                    
+            except requests.exceptions.RequestException as e:
+                logging.warning(f"Request failed: {e}")
+            except json.decoder.JSONDecodeError as e:
+                logging.warning(f"Failed to parse JSON: {e}. Response: {raw_json}")
+            except Exception as e:
+                logging.warning(f"Unexpected error occurred: {e}")
+
+            # Wait before the next retry
+            if retry < max_retries - 1:
+                sleep(delay)
+        
+        # If all retries fail, return an empty list
+        logging.warning(f"Couldn't fetch symbols after {max_retries} attempts.")
+        return []
+
+
     def get_auto_rotate_symbols(self, min_qty_threshold: float = None, whitelist: list = None, blacklist: list = None, max_usd_value: float = None, max_retries: int = 5):
         if self.rotator_symbols_cache and not self.is_cache_expired():
             return self.rotator_symbols_cache
