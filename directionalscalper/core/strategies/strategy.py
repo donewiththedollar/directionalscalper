2b93d62fd53d87d54b6eb23e3b09195a25e9819d
Tyler Simpson <donewiththedollar@gmail.com>
2023-12-16 13:43:59 +1100

    ERI trend verification 15m to avoid over counter trade

--- directionalscalper/core/strategies/strategy.py
+++ directionalscalper/core/strategies/strategy.py
@@ -3399,10 +3399,13 @@ class Strategy:
             eri_trend_aligned_long = eri_trend == "bullish"
             eri_trend_aligned_short = eri_trend == "bearish"
 
+            mfi_signal_long = mfi.lower() == "long"
+            mfi_signal_short = mfi.lower() == "short"
+            mfi_signal_neutral = mfi.lower() == "neutral"
+
             if one_minute_volume > min_vol:
                 # Long Entry for Trend and MFI Signal
-                mfi_signal_long = mfi.lower() == "long"
-                if (should_long or should_add_to_long) and current_price >= qfl_base and trend_aligned_long and mfi_signal_long:
+                if (should_long or should_add_to_long) and current_price >= qfl_base and eri_trend_aligned_long and mfi_signal_long:
                     if long_pos_qty == 0 and not self.entry_order_exists(open_orders, "buy"):
                         logging.info(f"Placing initial long entry for {symbol}")
                         self.place_postonly_order_bybit(symbol, "buy", long_dynamic_amount, best_bid_price, positionIdx=1, reduceOnly=False)
@@ -3412,8 +3415,7 @@ class Strategy:
                         time.sleep(5)
 
                 # Short Entry for Trend and MFI Signal
-                mfi_signal_short = mfi.lower() == "short"
-                if (should_short or should_add_to_short) and current_price <= qfl_ceiling and trend_aligned_short and mfi_signal_short:
+                if (should_short or should_add_to_short) and current_price <= qfl_ceiling and eri_trend_aligned_short and mfi_signal_short:
                     if short_pos_qty == 0 and not self.entry_order_exists(open_orders, "sell"):
                         logging.info(f"Placing initial short entry for {symbol}")
                         self.place_postonly_order_bybit(symbol, "sell", short_dynamic_amount, best_ask_price, positionIdx=2, reduceOnly=False)
@@ -3742,16 +3744,20 @@ class Strategy:
             trend_aligned_long = (eri_trend == "bullish" or trend.lower() == "long") and mfi.lower() == "long"
             trend_aligned_short = (eri_trend == "bearish" or trend.lower() == "short") and mfi.lower() == "short"
 
+            eri_trend_aligned_long = eri_trend == "bullish"
+            eri_trend_aligned_short = eri_trend == "bearish"
+
+
             if one_minute_volume > min_vol:
                 # Long Entry Logic
-                if should_long and long_pos_qty == 0 and trend_aligned_long and current_price >= qfl_base:
+                if should_long and long_pos_qty == 0 and eri_trend_aligned_long and current_price >= qfl_base:
                     if not self.entry_order_exists(open_orders, "buy"):
                         logging.info(f"Placing initial long entry for {symbol}")
                         entry_price = largest_bid_wall[0] if largest_bid_wall else best_bid_price
                         self.place_postonly_order_bybit(symbol, "buy", long_dynamic_amount, entry_price, positionIdx=1, reduceOnly=False)
 
                 # Short Entry Logic
-                if should_short and short_pos_qty == 0 and trend_aligned_short and current_price <= qfl_ceiling:
+                if should_short and short_pos_qty == 0 and eri_trend_aligned_short and current_price <= qfl_ceiling:
                     if not self.entry_order_exists(open_orders, "sell"):
                         logging.info(f"Placing initial short entry for {symbol}")
                         entry_price = largest_ask_wall[0] if largest_ask_wall else best_ask_price
