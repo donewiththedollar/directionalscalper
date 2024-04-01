477c9a7d17125a61d25d74a3ca3cb4207cb28c9f
Tyler Simpson <donewiththedollar@gmail.com>
2024-04-01 01:10:22 +0000

    Move

--- directionalscalper/core/strategies/base_strategy.py
+++ directionalscalper/core/strategies/base_strategy.py
@@ -690,12 +690,6 @@ class BaseStrategy:
         position_data = self.exchange.get_positions_bybit(self.symbol)
         return position_data
 
-    def calculate_next_update_time(self):
-        """Returns the time for the next TP update, which is 30 seconds from the current time."""
-        now = datetime.now()
-        next_update_time = now + timedelta(seconds=10)
-        return next_update_time.replace(microsecond=0)
-
     def calculate_short_take_profit_bybit(self, short_pos_price, symbol):
         if short_pos_price is None:
             return None
@@ -4709,52 +4703,6 @@ class BaseStrategy:
                 self.exchange.cancel_order_by_id(order['id'], symbol)
                 logging.info(f"Cancelled TP order {order['id']} for update.")
                 time.sleep(0.05)
-            except Exception as e:
-                logging.error(f"Error in cancelling {order_side} TP order {order['id']}. Error: {e}")
-
-        now = datetime.now()
-        if now >= last_tp_update or mismatched_qty_orders:
-            # Check if a TP order already exists
-            tp_order_exists = (order_side == "sell" and long_tp_count > 0) or (order_side == "buy" and short_tp_count > 0)
-
-            # Set new TP order with updated prices only if no TP order exists
-            if not tp_order_exists:
-                new_tp_price = new_long_tp if order_side == "sell" else new_short_tp
-                try:
-                    self.exchange.create_take_profit_order_bybit(symbol, "limit", order_side, pos_qty, new_tp_price, positionIdx=positionIdx, reduce_only=True)
-                    logging.info(f"New {order_side.capitalize()} TP set at {new_tp_price}")
-                except Exception as e:
-                    logging.error(f"Failed to set new {order_side} TP for {symbol}. Error: {e}")
-            else:
-                logging.info(f"Skipping TP update as a TP order already exists for {symbol}")
-
-            # Calculate and return the next update time
-            return self.calculate_next_update_time()
-        else:
-            logging.info(f"No immediate update needed for TP orders for {symbol}. Last update at: {last_tp_update}")
-            return last_tp_update
-
-    def update_quickscalp_tp(self, symbol, pos_qty, upnl_profit_pct, short_pos_price, long_pos_price, positionIdx, order_side, last_tp_update, tp_order_counts, max_retries=10):
-        # Fetch the current open TP orders and TP order counts for the symbol
-        long_tp_orders, short_tp_orders = self.exchange.get_open_tp_orders(symbol)
-        long_tp_count = tp_order_counts['long_tp_count']
-        short_tp_count = tp_order_counts['short_tp_count']
-
-        # Calculate the new TP values using quickscalp method
-        new_short_tp = self.calculate_quickscalp_short_take_profit(short_pos_price, symbol, upnl_profit_pct)
-        new_long_tp = self.calculate_quickscalp_long_take_profit(long_pos_price, symbol, upnl_profit_pct)
-
-        # Determine the relevant TP orders based on the order side
-        relevant_tp_orders = long_tp_orders if order_side == "sell" else short_tp_orders
-
-        # Check if there's an existing TP order with a mismatched quantity
-        mismatched_qty_orders = [order for order in relevant_tp_orders if order['qty'] != pos_qty and order['id'] not in self.auto_reduce_order_ids.get(symbol, [])]
-
-        # Cancel mismatched TP orders if any
-        for order in mismatched_qty_orders:
-            try:
-                self.exchange.cancel_order_by_id(order['id'], symbol)
-                logging.info(f"Cancelled TP order {order['id']} for update.")
             except Exception as e:
                 logging.error(f"Error in cancelling {order_side} TP order {order['id']}. Error: {e}")
 
