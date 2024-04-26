705838e17661090f39aeff83d1c0e6e4047f2f78
Tyler Simpson <donewiththedollar@gmail.com>
2024-04-26 02:46:49 +0000

    I fixed things

--- directionalscalper/core/strategies/bybit/notional/basicgridmfirsi_notional_buffer_quickscalp_dynamictp.py
+++ directionalscalper/core/strategies/bybit/notional/basicgridmfirsi_notional_buffer_quickscalp_dynamictp.py
@@ -719,7 +719,7 @@ class BybitBasicGridBufferedQSDTP(BybitStrategy):
                     short_tp_counts = tp_order_counts['short_tp_count']
 
                     try:
-                        self.linear_grid_handle_positions_mfirsi_persistent_notional_dynamic_buffer_qs_dynamictp(
+                        self.linear_grid_handle_positions_mfirsi_persistent_notional_dynamic_buffer_qs_dynamictp_arfull(
                             symbol,
                             open_symbols,
                             total_equity,
