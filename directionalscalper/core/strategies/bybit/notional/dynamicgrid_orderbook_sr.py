247e5f51c13e6a81fb4a6814723a80ab71caa133
Tyler Simpson <donewiththedollar@gmail.com>
2024-05-14 18:42:36 +0000

    I love the logs

--- directionalscalper/core/strategies/bybit/notional/dynamicgrid_orderbook_sr.py
+++ directionalscalper/core/strategies/bybit/notional/dynamicgrid_orderbook_sr.py
@@ -58,6 +58,8 @@ class BybitDynamicGridSpanOBSR(BybitStrategy):
             self.failsafe_start_pct = self.config.linear_grid['failsafe_start_pct']
             self.auto_reduce_cooldown_enabled = self.config.linear_grid['auto_reduce_cooldown_enabled']
             self.auto_reduce_cooldown_start_pct = self.config.linear_grid['auto_reduce_cooldown_start_pct']
+            self.max_qty_percent_long = self.config.linear_grid['max_qty_percent_long']
+            self.max_qty_percent_short = self.config.linear_grid['max_qty_percent_short']
             # self.reissue_threshold_inposition = self.config.linear_grid['reissue_threshold_inposition']
                                             #  failsafe_enabled,
                                             #  long_failsafe_upnl_pct,
@@ -197,7 +199,8 @@ class BybitDynamicGridSpanOBSR(BybitStrategy):
             failsafe_start_pct = self.config.linear_grid['failsafe_start_pct']
             auto_reduce_cooldown_enabled = self.config.linear_grid['auto_reduce_cooldown_enabled']
             auto_reduce_cooldown_start_pct = self.config.linear_grid['auto_reduce_cooldown_start_pct']
-            
+            max_qty_percent_long = self.config.linear_grid['max_qty_percent_long']
+            max_qty_percent_short = self.config.linear_grid['max_qty_percent_short']
 
             # reissue_threshold_inposition = self.config.linear_grid['reissue_threshold_inposition']
 
@@ -828,7 +831,9 @@ class BybitDynamicGridSpanOBSR(BybitStrategy):
                             upnl_profit_pct,
                             max_upnl_profit_pct,
                             tp_order_counts,
-                            entry_during_autoreduce
+                            entry_during_autoreduce,
+                            max_qty_percent_long,
+                            max_qty_percent_short
                         )
                     except Exception as e:
                         logging.info("Something is up with variables for the grid")
