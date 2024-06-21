d94bce39cb8c94477990c4f9bd73e7f78144dcd2
Tyler Simpson <donewiththedollar@gmail.com>
2024-06-21 19:09:34 +0000

    Fix import

--- directionalscalper/core/strategies/bybit/notional/instantsignals/__init__.py
+++ directionalscalper/core/strategies/bybit/notional/instantsignals/__init__.py
@@ -3,4 +3,4 @@ from .dynamicgrid_dynamictp_gridspan import BybitDynamicGridSpanIS
 from .dynamicgrid_oblevels_tight import BybitDynamicGridSpanOBTight
 from .dynamicgrid_oblevels import BybitDynamicGridSpanOBLevels
 from .qstrendob_dynamictp import BybitQuickScalpTrendDynamicTP
-from dynamicgrid_oblevels_spot import BybitSpotGridStrategy
+from .dynamicgrid_oblevels_spot import BybitSpotGridStrategy
