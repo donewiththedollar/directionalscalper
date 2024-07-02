493a71908797b036e87e82cce57cbb0933caa195
Tyler Simpson <donewiththedollar@gmail.com>
2024-07-02 20:36:31 +0000

    Rm double func fix old strat

--- directionalscalper/core/strategies/bybit/notional/instantsignals/qstrendob_dynamictp.py
+++ directionalscalper/core/strategies/bybit/notional/instantsignals/qstrendob_dynamictp.py
@@ -455,6 +455,23 @@ class BybitQuickScalpTrendDynamicTP(BybitStrategy):
                 logging.info(f"Current long pos qty for {symbol} {long_pos_qty}")
                 logging.info(f"Current short pos qty for {symbol} {short_pos_qty}")
 
+                # Check if a position has been closed
+                if previous_long_pos_qty > 0 and long_pos_qty == 0:
+                    logging.info(f"Long position closed for {symbol}. Canceling long grid orders.")
+                    self.cancel_grid_orders(symbol, "buy")
+                    if short_pos_qty == 0:
+                        logging.info(f"No open positions for {symbol}. Removing from shared symbols data.")
+                        shared_symbols_data.pop(symbol, None)
+                    break  # Exit the while loop, thus ending the thread
+
+                elif previous_short_pos_qty > 0 and short_pos_qty == 0:
+                    logging.info(f"Short position closed for {symbol}. Canceling short grid orders.")
+                    self.cancel_grid_orders(symbol, "sell")
+                    if long_pos_qty == 0:
+                        logging.info(f"No open positions for {symbol}. Removing from shared symbols data.")
+                        shared_symbols_data.pop(symbol, None)
+                    break  # Exit the while loop, thus ending the thread
+
                 # Check for position inactivity
                 inactive_pos_time_threshold = 180  # 3 minutes in seconds
                 if self.check_position_inactivity(symbol, inactive_pos_time_threshold, long_pos_qty, short_pos_qty, previous_long_pos_qty, previous_short_pos_qty):
