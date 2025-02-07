971be58e107c4e2152de0281bfc88830b90b5b70
Sides <sidesamitss@gmail.com>
2025-02-08 01:37:29 +0200

    bybit balance fix & None whitelist check

--- api/manager.py
+++ api/manager.py
@@ -358,7 +358,7 @@ class Manager:
                             logging.debug(f"Skipping {symbol} as it's in blacklist")
                             continue
 
-                        if len(whitelist) > 0 and symbol not in whitelist:
+                        if whitelist and len(whitelist) > 0 and symbol not in whitelist:
                             logging.debug(f"Skipping {symbol} as it's not in whitelist")
                             continue
 
